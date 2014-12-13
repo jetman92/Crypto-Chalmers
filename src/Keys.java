5dd78bfbcc124b7c028c9941e956ea04ed32550e
jetman92 <arnaud.dartigues@gmail.com>
2014-12-13 12:37:20 +0100

    Keys done

--- src/Keys.java
+++ src/Keys.java
@@ -1,4 +1,6 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class Keys {
@@ -9,7 +11,27 @@ public class Keys {
 	
 	public Keys(DSACommunity com) {
 		BigInteger q = com.getQ();
-		SecureRandom priv = new SecureRandom();
+		SecureRandom random = null;
+		try {
+			random = SecureRandom.getInstance("SHA1PRNG");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] privkey = new byte[20];
+		privateKey = q;
+		while( ((q.compareTo(privateKey) == -1) || (q.compareTo(privateKey) == 0))){
+			random.nextBytes(privkey);
+			privateKey = new BigInteger(privkey).abs();
+		}
+		
+		BigInteger g = com.getG();
+		BigInteger p = com.getP();
+		
+		publicKey = g.modPow(privateKey, p);
+		
+		System.out.println(privateKey.toString());
+		System.out.println(publicKey.toString());
 		
 		
 	}
